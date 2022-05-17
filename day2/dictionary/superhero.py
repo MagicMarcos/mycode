5fb86961dd7a241b00a81856dd48455f530f182a
Marcos <magicmarcos@users.noreply.github.com>
2022-05-17 22:26:19 +0000

    About complex lists

---
+++ day2/dictionary/superhero.py
@@ -0,0 +1,22 @@
+#!usr/bin/env python3
+
+superhero={
+        "name" : "Professor Charles Xavier",
+        "aliases" : [
+                      "Professor X", 
+                      "Onslaught", 
+                      "Consort-Royal", 
+                      "Founder", 
+                      "X", 
+                      "Warlord", 
+                      "Entity"
+            ],
+        "base of operations" : "Salem Center, New York", 
+        "creator" : [
+            "Stan Lee", 
+            "Jack Kirby"
+            ], 
+        "bio" : "Professor X (Professor Charles Xavier) is the founder of the superhero team The X-Men in the Marvel Universe and an incredibly powerful mutant hero."
+        }
+
+print(superhero)
